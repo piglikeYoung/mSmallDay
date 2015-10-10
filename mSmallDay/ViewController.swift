afc057d9ee80953470f2a799c433867ca358cc79
piglikeyoung <316046510@qq.com>
2015-10-10 11:25:59 +0800

    Initial Commit

---
+++ mSmallDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mSmallDay
+//
+//  Created by piglikeyoung on 15/10/10.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
